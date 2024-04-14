ec9cf4c98748f3316dafd1c0f8f1b1d44a122be9
Panicshoe <86228058+Panicshoe@users.noreply.github.com>
2024-04-14 01:10:48 -0400

    react-frontend-modified

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
